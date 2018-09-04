8f336539bb20405a1a25d4e474701caa61f5f840
nagmabano <bano.nagma.1@gmail.com>
2018-09-04 16:09:00 +0530

    swap two numbers

--- add.js
+++
@@ -1,8 +0,0 @@
-function add(){
-    var a,b,c;
-    var a = Number(document.getElementById("first-add").value);
-    var b = Number(document.getElementById("second-add").value);
-    var c = a + b;
-    document.getElementById("answer").innerText = c;
-}
-
--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 function hello(){
     alert("Hello World!!!!");
   }
+
+function add(){
+    var a,b,c;
+    var a = Number(document.getElementById("first-add").value);
+    var b = Number(document.getElementById("second-add").value);
+    var c = a + b;
+    document.getElementById("answer").innerText = c;
+}
+
+function swap(){
+  var a,b,c;
+  var a = Number(document.getElementById("first-swap").value);
+  var b = Number(document.getElementById("second-swap").value);
+  c = b;
+  b = a;
+  a = c;
+  document.getElementById("answer1").innerText = a;
+  document.getElementById("answer2").innerText = b;
+}
